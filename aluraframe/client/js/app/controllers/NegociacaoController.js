6bc2348deebd86459206a0e4c741b7d0973ff12b
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2017-10-25 14:21:09 -0200

    04. chame um ajudante

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,39 +7,36 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event) {
         event.preventDefault();
+        
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
 
-        console.log(typeof(this._inputData.value));
-        console.log(this._inputData.value);
+        this._listaNegociacoes.negociacoes.length = 0;
+        console.log(this._listaNegociacoes.negociacoes);
 
-        let data = new Date(
-            ...this._inputData.value
-            .split('-')
-            .map((item, indice) => item - indice % 2)
-        );
-        console.log(data);
-
-        let negociacao = new Negociacao(
-            data,
-            this._inputQuantidade.value,
-            this._inputValor.value
-        );
-
-        console.log(negociacao);
-
-        this.limpaFormulario();
     }
 
-    limpaFormulario() {
-        console.log('limpaFormulario');
+    _criaNegociacao() {
+        
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );        
+    }
+
+    _limpaFormulario() {
 
         this._inputData.value = '';
-        this._inputQuantidade.value = '';
-        this._inputValor = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
 
         this._inputData.focus();
     }
+    
 }
