8c00e31c12d51c0d3cc87c2bb187b8890b052515
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2017-10-26 15:45:13 -0200

    05. temos o modelo, mas e a view?

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,27 +7,30 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+
         this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+
+        this._negociacoesView.update(this._listaNegociacoes);
     }
 
     adiciona(event) {
+
         event.preventDefault();
-        
+
         this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
+
         this._limpaFormulario();
-
-        this._listaNegociacoes.negociacoes.length = 0;
-        console.log(this._listaNegociacoes.negociacoes);
-
     }
 
     _criaNegociacao() {
-        
+
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
-        );        
+        );
     }
 
     _limpaFormulario() {
@@ -38,5 +41,5 @@ class NegociacaoController {
 
         this._inputData.focus();
     }
-    
+
 }
