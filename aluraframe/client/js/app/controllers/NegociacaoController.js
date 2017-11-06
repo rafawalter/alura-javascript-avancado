5459858c15cb40b33dcf7b083d591fbfe60fc533
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2017-11-06 12:59:16 -0200

    04. Importando negociações

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -22,6 +22,21 @@ class NegociacaoController {
 
     }
 
+    importaNegociacoes() {
+
+        let service = new NegociacaoService();
+
+        service.obterNegociacoesDaSemana((erro, negociacoes) => {
+            if (erro) {
+                this._mensagem.texto = erro;
+                return;
+            }
+
+            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = 'Negociações importadas com sucesso';
+        });
+    }
+
     adiciona(event) {
 
         event.preventDefault();
