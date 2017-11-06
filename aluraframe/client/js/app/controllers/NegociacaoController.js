6983fbff18774a2e110ab38692500f5ebee03f1a
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2017-11-06 10:49:02 -0200

    03. E se alguém criasse nossos Proxies? O padrão de projeto Factory

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -8,32 +8,18 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        let self = this;
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
-            get(target, prop, receiver) {
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(),
+            new NegociacoesView($('#negociacoesView')),
+            'adiciona', 'esvazia'
+        );
 
-                if (['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)) {
+        this._mensagem = new Bind(
+            new Mensagem(),
+            new MensagemView($('#mensagemView')),
+            'texto'
+        );
 
-                    return function() {
-
-                        console.log(`método '${prop}' interceptado`);
-
-                        Reflect.apply(target[prop], target, arguments);
-
-                        self._negociacoesView.update(target);
-                    };
-                }
-
-                return Reflect.get(target, prop, receiver);
-            }
-        });
-
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
-
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event) {
@@ -43,17 +29,13 @@ class NegociacaoController {
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._mensagem.texto = 'Negociação adicionada com sucesso';
 
-        this._mensagemView.update(this._mensagem);
-
         this._limpaFormulario();
     }
 
     apaga() {
 
         this._listaNegociacoes.esvazia();
-
         this._mensagem.texto = 'Negociações apagadas com sucesso';
-        this._mensagemView.update(this._mensagem);
     }
 
     _criaNegociacao() {
