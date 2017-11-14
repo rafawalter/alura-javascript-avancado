c49c0a38130de336ca79e6c837cdd66d7ccebec6
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2017-11-14 20:25:38 -0200

    ordenando ao clicar numa coluna

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -8,10 +8,12 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
+        this._ordemAtual = '';
+
         this._listaNegociacoes = new Bind(
             new ListaNegociacoes(),
             new NegociacoesView($('#negociacoesView')),
-            'adiciona', 'esvazia'
+            'adiciona', 'esvazia', 'ordena', 'inverteOrdem'
         );
 
         this._mensagem = new Bind(
@@ -50,6 +52,17 @@ class NegociacaoController {
         this._listaNegociacoes.esvazia();
         this._mensagem.texto = 'Negociações apagadas com sucesso';
     }
+
+    ordena(coluna) {
+        if (this._ordemAtual == coluna) {
+            this._listaNegociacoes.inverteOrdem();
+        } else {
+            this._listaNegociacoes.ordena((a, b) => a[coluna] - b[coluna]);
+            this._ordemAtual = coluna;
+        }
+    }
+
+
 
     _criaNegociacao() {
 
